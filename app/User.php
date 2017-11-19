0751bf6debc080e74d5a323d6c717854930bfcd8
thaiduc96 <huynhjduc248@gmail.com>
2017-11-20 00:54:23 +0700

    'sua_sp_admin'

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'full_name', 'email', 'password','level','gender','address','phone_number','note,'
+        'full_name', 'email', 'password','level','gender','address','phone_number','note'
     ];
 
     /**
