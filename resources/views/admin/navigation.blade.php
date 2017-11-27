fd6b4a0213f0a40c6a7061739855b7f061ce242c
ckcuitcto <huynhjduc248@gmail.com>
2017-11-27 23:41:32 +0700

    Update navigation.blade.php

--- resources/views/admin/navigation.blade.php
+++ resources/views/admin/navigation.blade.php
@@ -28,7 +28,7 @@
                 </ul>
             </li>
             <li>
-                <a href=""><i class="fa fa-sitema   p fa-3x"></i>Người dùng<span class="fa arrow"></span></a>
+                <a href=""><i class="fa fa-sitemap fa-3x"></i>Người dùng<span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
                         <a href="index.php?p=listUser">Danh sách người dùng</a>
