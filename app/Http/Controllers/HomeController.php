b9c9c0e78dc100090d03a9f5c23c8bac5c45aba9
hanie <gia-han.tranngoc@ant-tech.eu>
2017-11-28 14:29:24 +0700

    admin slide chua sua duoc phan hinh anh, admin user moi lam hien thi va sua

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,62 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Slide;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return view('home');
     }
+
+
+// Slide
+    public function getSlide(){
+        $slide = Slide::orderBy('slide.id', 'DESC')->get();
+        return view('admin.slide.slide',compact('slide'));
+    }
+
+    public function getEdit($id){
+        $slide = Slide::find($id);
+        return view('admin.slide.edit',compact('slide'));
+    }
+
+    public function postEdit(Request $request, $id){
+        $slide = Slide::find($id);
+        if ($request->hasFile('fImages')) {
+            $file = $request->file('fImages');
+
+            $fileExtensions = $file->getClientOriginalExtension();
+            if (!$this->checkExtension($fileExtensions)) {
+                return redirect()->reload('admin.slide.getEdit')->with('failed', 'Chỉ được chọn file có đuôi jpg,png,jpeg');
+            }
+
+            $fileName = str_random(8) . "_" . $file->getClientOriginalName();
+            while (file_exists("template/image/slide/" . $fileName)) {
+                $fileName = str_random(8) . "_" . $file->getClientOriginalName();
+            }
+            $file->move('template/image/slide/', $fileName);
+            unlink("template/image/slide/" . $slide->image);
+            $slide->image = $fileName;
+        }
+        $slide->save();
+
+        
+        return redirect()->route('admin.slide.list')->with(['flash_message' => 'Sửa thành công']);
+    }
+
+    private function checkExtension($fileExtensions)
+    {
+        $arr = array('jpg', 'png', 'jpeg');
+        if (in_array($fileExtensions, $arr)) {
+            return true;
+        }
+        return false;
+    }
+//end Slide
 }
--- resources/views/admin/slide/slide.blade.php
+++ resources/views/admin/slide/slide.blade.php
@@ -6,17 +6,6 @@
         
     </div>
 </div>
-<div class="row">
-    <div class="col-md-4 donhang" xacnhan="2">
-     <h3><a href="#">Tất cả đơn hàng</a></h3>     
-    </div>
-    <div class="col-md-4 donhang" xacnhan="1">
-     <h3><a href="#">Đơn hàng đã xác nhận</a> </h3>     
-    </div>
-    <div class="col-md-4 donhang" xacnhan="0">
-     <h3><a href="#">Đơn hàng chưa nhận </a></h3>     
-    </div>
-</div>
 
 <div class="row">
     <div class="col-md-12 col-xs-12 col-sm-12">
@@ -24,37 +13,23 @@
          <table class="table table-striped table-bordered table-hover" id="dataTables-example">
             <thead>
               <tr>
-                <th style="text-align: center">STT</th>
-                <th style="text-align: center">ID Đơn Hàng</th>
-                <th style="text-align: center">Tên người nhận</th>
-                <th style="text-align: center">Nơi nhận</th>
-                <th style="text-align: center" >Số điện thoại</th>
-                 <th style="text-align: center" >Ghi chú</th>
-                <th style="text-align: center">Số tiền (VNĐ)</th>
-                <th style="text-align: center">Tình trạng</th>
+                <th style="text-align: center">ID</th>
+                <th style="text-align: center">Link</th>
+                <th style="text-align: center">Image</th>
               </tr>
             </thead>
 
             <tbody >
-           
+           @foreach($slide as $sl)
               <tr >
-                <td style="text-align: center"></td>
-                <td style="text-align: center"></td>
-                <td style="text-align: center"></td>
-                <td style="text-align: center"></td>
-
-                <td style="text-align: center"></td>
-                 <td style="text-align: center"></td>
-                <td style="text-align: center"></td>
-                <td style="text-align: center">
-                 
+                <td style="text-align: center"><a href="{{route('admin.slide.getEdit',$sl->id)}}">{{$sl->id}}</a></td>
+                <td style="text-align: center">{{$sl->link}}</td>
+                <td style="text-align: center">{{$sl->image}}</td>                 
                 </td>
               </tr>
-            
+            @endforeach
             </tbody>
-         
           </table>
-
           <div id="ketquaz"></div>
     </div>
     </div>
