6418972fc24858752d0b23d4bbebf4c6e0fd2ce5
thaiduc96 <huynhjduc248@gmail.com>
2017-12-04 01:27:46 +0700

    introduce , footer

---
+++ app/Introduce.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Introduce extends Model
+{
+    protected $table = "introduce";
+
+    protected $fillable = ['title','content'];
+
+}
