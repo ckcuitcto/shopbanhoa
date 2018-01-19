e5bc9fce306d286554d5eaac654d9cc7bb25a30a
hanie <gia-han.tranngoc@ant-tech.eu>
2018-01-19 11:35:44 +0700

    giao dien trang san pham xong, còn thanh phân trang chưa làm

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,24 +1,24 @@
 <footer class="footer">
     <div class="row">
-        <div class="col-md-2">
-            <h5>Tài khoản của bạn</h5>
+        <div class="col-md-3">
+            <ul style="list-style-type: none"><h5>Tài khoản</h5>
             @foreach($account as $acc)
-            <a href="#">{{$acc}}</a><br>
-            @endforeach
+            <li>{{$acc}}</li>
+            @endforeach</ul>
         </div>
         <div class="col-md-2">
-            <h5>Thông Tin</h5>
+            <ul style="list-style-type: none"><h5>Thông Tin</h5>
             @foreach($info as $acc)
-            <a href="#">{{$acc}}</a><br>
-            @endforeach
+            <li>{{$acc}}</li>
+            @endforeach</ul>
         </div>
         <div class="col-md-2">
-            <h5>Nguồn</h5>
+            <ul style="list-style-type: none"><h5>Nguồn</h5>
             @foreach($source as $acc)
-            <a href="#">{{$acc}}</a><br>
-            @endforeach
+            <li>{{$acc}}</li>
+            @endforeach</ul>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-5">
             <h5>Một vài thông tin về trang web</h5>
             <p>{!!$description!!}</p>
         </div>
